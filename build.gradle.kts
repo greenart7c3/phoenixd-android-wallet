8a9afd5fb91509df358cc7efc3a8c15ae717cd64
greenart7c3 <greenart7c3@proton.me>
2024-07-29 09:04:04 -0300

    add git hooks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -5,4 +7,38 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.serialization) apply false
+}
+
+tasks.register<Copy>("installGitHook") {
+    from(File(rootProject.rootDir, "git-hooks/pre-commit"))
+    from(File(rootProject.rootDir, "git-hooks/pre-push"))
+    into(File(rootProject.rootDir, ".git/hooks"))
+    fileMode = 0b111101101
+}
+
+
+tasks.getByPath(":app:preBuild").dependsOn(tasks.getByName("installGitHook"))
+
+subprojects {
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            val buildDirPath = layout.buildDirectory.get().asFile.absolutePath
+            if (project.findProperty("composeCompilerReports") == "true") {
+                freeCompilerArgs.addAll(
+                    listOf(
+                        "-P",
+                        "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=$buildDirPath/compose_compiler"
+                    )
+                )
+            }
+            if (project.findProperty("composeCompilerMetrics") == "true") {
+                freeCompilerArgs.addAll(
+                    listOf(
+                        "-P",
+                        "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=$buildDirPath/compose_compiler"
+                    )
+                )
+            }
+        }
+    }
 }
