59198de606f16c80d4715a54f7df9d230a64fb05
greenart7c3 <greenart7c3@proton.me>
2024-07-31 11:10:18 -0300

    adjust padding

--- app/src/main/java/com/greenart7c3/phoenixd/screens/MainScreen.kt
+++ app/src/main/java/com/greenart7c3/phoenixd/screens/MainScreen.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
@@ -75,6 +78,7 @@ fun MainScreen(
 
             if (showChannels) {
                 ModalBottomSheet(
+                    modifier = Modifier.padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()),
                     sheetState = sheetState,
                     onDismissRequest = {
                         scope.launch {
