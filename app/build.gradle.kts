f9641c35db421f0fbdfc8cb304388ab586887460
greenart7c3 <greenart7c3@proton.me>
2024-09-20 09:57:08 -0300

    0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.greenart7c3.phoenixd"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "0.0.9"
+        versionCode = 10
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
